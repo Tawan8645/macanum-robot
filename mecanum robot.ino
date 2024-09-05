0c5f8b78c2eede2aeeebd8530f11cec398f17560
Tawan.DEV <88831519+Tawan8645@users.noreply.github.com>
2024-09-06 02:22:37 +0700

    Update mecanum robot.ino

--- mecanum robot.ino
+++ mecanum robot.ino
@@ -1,30 +1,45 @@
-//############################ mecanum robot by Tawan ATG ROBOT SHOP ################################ 
+// int ch1 = 14;
+// int ch2 = 15;
+// int ch3 = 16;
+// int ch4 = 17;
+// int ch5 = 18;
+// //value
+// int value1;
+// int value2;
+// int value3;
+// int value4;
+// int value5;
+
 //#### Motor Driver 1 ####
-int INA1 = 2;
-int INB1 = 3;
-int PWM1 = 4;
+int INA1 = 12;
+int INB1 = 14;
+int PWM1 = 27;
 //#### Motor Driver 2 ####
-int INA2 = 5;
-int INB2 = 6;
-int PWM2 = 7;
+int INA2 = 25;
+int INB2 = 33;
+int PWM2 = 32;
 //#### Motor Driver 3 ####
-int INA3 = 8;
-int INB3 = 9;
-int PWM3 = 10;
+int INA3 = 15;
+int INB3 = 2;
+int PWM3 = 0;
 //#### Motor Driver 4 ####
-int INA4 = 11;
-int INB4 = 12;
-int PWM4 = 13;
+int INA4 = 5;
+int INB4 = 18;
+int PWM4 = 19;
 
 
 //#### speed bord ####
-int speed1 = 255 ; 
-int speed2 = 255 ; 
-int speed3 = 255 ; 
-int speed4 = 255 ; 
-
+int speed1 = 100;
+int speed2 = 100;
+int speed3 = 100;
+int speed4 = 100;
 
 void setup() {
+  // pinMode(ch1, INPUT);
+  // pinMode(ch2, INPUT);
+  // pinMode(ch3, INPUT);
+  // pinMode(ch4, INPUT);
+  // pinMode(ch5, INPUT);
   //#### Motor Driver 1 ####
   pinMode(INA1, OUTPUT);
   pinMode(INB1, OUTPUT);
@@ -41,55 +56,78 @@ void setup() {
   pinMode(INA4, OUTPUT);
   pinMode(INB4, OUTPUT);
   pinMode(PWM4, OUTPUT);
+
+
+  Serial.begin(9600);
 }
 
 void loop() {
- forword();
- delay(3000);
- backword();
- delay(3000);
- turnRight();
- delay(3000);
- turnLeft();
- delay(3000);
- slideRight();
- delay(3000);
- slideLeft();
- delay(3000);
- forwordRight();
- delay(3000);
- forwordLeft();
- delay(3000);
- backwordRight();
- delay(3000);
- backwordLeft();
- delay(3000);
- stopbot();
- delay(3000);
+
+  // value1 = pulseIn(ch1, HIGH);  // อ่านค่าความกว้างของสัญญาณของแต่ละ Channel
+  // value2 = pulseIn(ch2, HIGH);
+  // value3 = pulseIn(ch3, HIGH);
+  // value4 = pulseIn(ch4, HIGH);
+  // value5 = pulseIn(ch5, HIGH);
+
+
+  // Serial.print("Channel 1:");
+  // Serial.println(value1);
+
+  // Serial.print("Channel 2:");
+  // Serial.println(value2);
+  // Serial.print("Channel 3:");
+  // Serial.println(value3);
+  // Serial.print("Channel 4:");
+  // Serial.println(value4);
+  // Serial.print("Channel 5:");
+  // Serial.println(value5);
+
+
+  // if (value2 < 1100) {
+  //   Serial.println("เดินหน้า");
+  //   backword();
+
+  // } else if (value2 > 1900) {
+  //   Serial.println("ถอยหลัง");
+  //   forword();
+  // } else if (value1 > 1900) {
+  //   Serial.println("ขวา");
+  //   slideRight();
+  // } else if (value1 < 1100) {
+  //   Serial.println("ซ้าย");
+  //   slideLeft();
+  // } else if (value4 > 1900) {
+  //   Serial.println("ขวา");
+  //   turnRight();
+  // } else if (value4 < 1100) {
+  //   Serial.println("ซ้าย");
+  //   turnLeft();
+  // } else if ((value2 > 1600) && (value1 > 1600)) {
+  //   Serial.println("เดินหน้าขวา");
+  //   forwordRight();
+
+  // } else if ((value2 > 1600) && (value1 < 1400)) {
+  //   Serial.println("เดินหน้าซ้้าย");
+  //   forwordLeft();
+
+  // } else if ((value2 < 1400) && (value1 > 1600)) {
+  //   Serial.println("เดินหน้าซ้้าย");
+  //   backwordRight();
+
+  // } else if ((value2 < 1400) && (value1 < 1400)) {
+  //   Serial.println("เดินหน้าซ้้าย");
+  //   backwordLeft();
+
+  // }
+
+
+
+  // else {
+  //   Serial.println("หยุด");
+  //   stopbot();
+  // }
 }
-
-
-///////////////////////////////////////////////
 void forword() {
-  //#### Motor Driver 1 ####
-  digitalWrite(INA1, 0);
-  digitalWrite(INB1, 1);
-  analogWrite(PWM1, speed1);
-  //#### Motor Driver 2 ####
-  digitalWrite(INA2, 1);
-  digitalWrite(INB2, 0);
-  analogWrite(PWM2, speed2);
-  //#### Motor Driver 3 ####
-  digitalWrite(INA3, 0);
-  digitalWrite(INB3, 1);
-  analogWrite(PWM3, speed3);
-  //#### Motor Driver 4 ####
-  digitalWrite(INA4, 1);
-  digitalWrite(INB4, 0);
-  analogWrite(PWM4, speed4);
-}
-//////////////////////////////////////////////
-void backword() {
   //#### Motor Driver 1 ####
   digitalWrite(INA1, 1);
   digitalWrite(INB1, 0);
@@ -108,6 +146,25 @@ void backword() {
   analogWrite(PWM4, speed4);
 }
 /////////////////////////////////////////////
+void backword() {
+  //#### Motor Driver 1 ####
+  digitalWrite(INA1, 0);
+  digitalWrite(INB1, 1);
+  analogWrite(PWM1, speed1);
+  //#### Motor Driver 2 ####
+  digitalWrite(INA2, 1);
+  digitalWrite(INB2, 0);
+  analogWrite(PWM2, speed2);
+  //#### Motor Driver 3 ####
+  digitalWrite(INA3, 0);
+  digitalWrite(INB3, 1);
+  analogWrite(PWM3, speed3);
+  //#### Motor Driver 4 ####
+  digitalWrite(INA4, 1);
+  digitalWrite(INB4, 0);
+  analogWrite(PWM4, speed4);
+}
+//////////////////////////////////////////////
 void turnRight() {
   //#### Motor Driver 1 ####
   digitalWrite(INA1, 1);
@@ -183,46 +240,8 @@ void slideLeft() {
   digitalWrite(INB4, 0);
   analogWrite(PWM4, speed4);
 }
-/////////////////////////////////////////
-void forwordRight() {
-  //#### Motor Driver 1 ####
-  digitalWrite(INA1, 0);
-  digitalWrite(INB1, 0);
-  analogWrite(PWM1, speed1);
-  //#### Motor Driver 2 ####
-  digitalWrite(INA2, 1);
-  digitalWrite(INB2, 0);
-  analogWrite(PWM2, speed2);
-  //#### Motor Driver 3 ####
-  digitalWrite(INA3, 0);
-  digitalWrite(INB3, 1);
-  analogWrite(PWM3, speed3);
-  //#### Motor Driver 4 ####
-  digitalWrite(INA4, 0);
-  digitalWrite(INB4, 0);
-  analogWrite(PWM4, speed4);
-}
-////////////////////////////////////////
-void forwordLeft() {
-  //#### Motor Driver 1 ####
-  digitalWrite(INA1, 0);
-  digitalWrite(INB1, 1);
-  analogWrite(PWM1, speed1);
-  //#### Motor Driver 2 ####
-  digitalWrite(INA2, 0);
-  digitalWrite(INB2, 0);
-  analogWrite(PWM2, speed2);
-  //#### Motor Driver 3 ####
-  digitalWrite(INA3, 0);
-  digitalWrite(INB3, 0);
-  analogWrite(PWM3, speed3);
-  //#### Motor Driver 4 ####
-  digitalWrite(INA4, 1);
-  digitalWrite(INB4, 0);
-  analogWrite(PWM4, speed4);
-}
 ///////////////////////////////////////
-void backwordRight() {
+void forwordRight() {
   //#### Motor Driver 1 ####
   digitalWrite(INA1, 1);
   digitalWrite(INB1, 0);
@@ -241,7 +260,7 @@ void backwordRight() {
   analogWrite(PWM4, speed4);
 }
 //////////////////////////////////////
-void backwordLeft() {
+void forwordLeft() {
   //#### Motor Driver 1 ####
   digitalWrite(INA1, 0);
   digitalWrite(INB1, 0);
@@ -260,6 +279,44 @@ void backwordLeft() {
   analogWrite(PWM4, speed4);
 }
 ///////////////////////////////////
+void backwordRight() {
+  //#### Motor Driver 1 ####
+  digitalWrite(INA1, 0);
+  digitalWrite(INB1, 0);
+  analogWrite(PWM1, speed1);
+  //#### Motor Driver 2 ####
+  digitalWrite(INA2, 1);
+  digitalWrite(INB2, 0);
+  analogWrite(PWM2, speed2);
+  //#### Motor Driver 3 ####
+  digitalWrite(INA3, 0);
+  digitalWrite(INB3, 1);
+  analogWrite(PWM3, speed3);
+  //#### Motor Driver 4 ####
+  digitalWrite(INA4, 0);
+  digitalWrite(INB4, 0);
+  analogWrite(PWM4, speed4);
+}
+////////////////////////////////////////
+void backwordLeft() {
+  //#### Motor Driver 1 ####
+  digitalWrite(INA1, 0);
+  digitalWrite(INB1, 1);
+  analogWrite(PWM1, speed1);
+  //#### Motor Driver 2 ####
+  digitalWrite(INA2, 0);
+  digitalWrite(INB2, 0);
+  analogWrite(PWM2, speed2);
+  //#### Motor Driver 3 ####
+  digitalWrite(INA3, 0);
+  digitalWrite(INB3, 0);
+  analogWrite(PWM3, speed3);
+  //#### Motor Driver 4 ####
+  digitalWrite(INA4, 1);
+  digitalWrite(INB4, 0);
+  analogWrite(PWM4, speed4);
+}
+//////////////////////////////////////
 void stopbot() {
   //#### Motor Driver 1 ####        
   digitalWrite(INA1, LOW);
